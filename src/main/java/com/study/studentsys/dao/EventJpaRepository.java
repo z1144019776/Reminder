f64bd32e4a5a7ef57ab709ebef4616339ad4764a
jialubai <102417299+jialubai@users.noreply.github.com>
2022-04-20 11:33:25 +0800

    Delete src directory

--- src/main/java/com/study/studentsys/dao/EventJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.study.studentsys.dao;
-
-import com.study.studentsys.model.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventJpaRepository extends JpaRepository<Event, Long> {
-
-}
